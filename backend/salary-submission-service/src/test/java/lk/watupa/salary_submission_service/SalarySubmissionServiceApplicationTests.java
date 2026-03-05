4e445844d3911fbe4f2d4a3aaf13cf6a60ef8cc2
nipunsperera <samadinipun@gmail.com>
2026-03-05 09:36:21 +0530

    feature/salary_submission_service-Initial commit

---
+++ backend/salary-submission-service/src/test/java/lk/watupa/salary_submission_service/SalarySubmissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.watupa.salary_submission_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalarySubmissionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
