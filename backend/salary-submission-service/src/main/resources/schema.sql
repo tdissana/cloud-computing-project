4a334164c082bdf5b6a768ae682ac12842a2c508
nipunsperera <samadinipun@gmail.com>
2026-03-22 10:55:29 +0530

    changes to schema and change fields in table fields

--- backend/salary-submission-service/src/main/resources/schema.sql
+++ backend/salary-submission-service/src/main/resources/schema.sql
@@ -1,18 +1,20 @@
 CREATE SCHEMA IF NOT EXISTS salary;
 
-DROP TABLE IF EXISTS salary.submissions;
+DROP TABLE IF EXISTS salary.submission;
 
-CREATE TABLE salary.submissions
+CREATE TABLE salary.submission
 (
-    id                 VARCHAR(36) PRIMARY KEY,
-    company            VARCHAR(255),
-    role               VARCHAR(255),
+    id                 INT AUTO_INCREMENT PRIMARY KEY,
+    company_name       VARCHAR(255),
+    job_title          VARCHAR(255),
     experience_level   VARCHAR(50),
+    seniority          INT,
     country            VARCHAR(100) DEFAULT 'Sri Lanka',
+    currency           VARCHAR(10)  DEFAULT 'LKR',
     total_compensation DOUBLE,
     base_salary        DOUBLE,
-    currency           VARCHAR(10)  DEFAULT 'LKR',
+    skills             TEXT,
     anonymize          BOOLEAN      DEFAULT TRUE,
     status             VARCHAR(20)  DEFAULT 'PENDING',
-    created_at         TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
+    timestamp          TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
 );
